c3731c4fdd768097d59b664ded9758889954357d
Lawrence Peng <lawrencepeng0810@gmail.com>
2019-12-28 23:32:30 +0800

    initial commit

---
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        R.id.hello_main;
+        R.drawable.pig;
+        R.string.ok;
+        R.color.messageColor;
+
+    }
+}
